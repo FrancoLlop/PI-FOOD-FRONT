ac1f5d470119a96da786238936004c2b4c007c47
Franco <francollopard@gmail.com>
2023-02-15 14:19:48 -0300

    cambios en landingPage

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -2,22 +2,20 @@ import React, {useState} from 'react';
 import {Link} from 'react-router-dom'
 import './LandingPage.css'
 import logo from '../Resources/foodlogo.webp'
-import video from '../Resources/Video.mp4'
-
+import imageback from '../Resources/fondo.jpg'
  function LandingPage(){
     
     const [logotimer, setLogotimer] = useState(false)
 
     setTimeout(() =>{
     setLogotimer(true)
-}, 6000)
+}, 3000)
 
     return (
         <div className='landingPage'>
 
-            <video className='video' autoPlay loop muted>
-                <source src={video} type='video/mp4'></source>
-            </video>
+            <img className='imageback' src={imageback} alt='imageback'/>
+           
 
             <Link to={'/home'}><img className={logotimer ? 'logo' : 'logoBack'} src={logo} alt={'logo'}/></Link>
 
