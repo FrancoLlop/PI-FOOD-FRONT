8ac60bcedb449d35a724792fdda5c3d2a107e2c9
Franco <francollopard@gmail.com>
2023-08-16 16:12:18 -0300

    modified:   .gitignore 	deleted:    build/static/css/main.2c9ba8c1.css 	deleted:    build/static/css/main.2c9ba8c1.css.map 	deleted:    build/static/js/main.320f59bc.js 	deleted:    build/static/js/main.320f59bc.js.LICENSE.txt 	deleted:    build/static/js/main.320f59bc.js.map 	deleted:    build/static/media/Video.455d45018dbc24d76c4a.mp4 	modified:   src/index.js

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import {Provider} from 'react-redux'
 import axios from 'axios'
 
 // axios.defaults.baseURL = 'http://localhost:3001'  
-axios.defaults.baseURL = 'https://pi-food-back-production-92ed.up.railway.app'  
+axios.defaults.baseURL = 'https://food-back-qgzr.onrender.com'  
 
 ReactDOM.render(
   <Provider store={store}>
